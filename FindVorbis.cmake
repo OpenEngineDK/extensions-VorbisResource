3e587f09f88fe00c104472285042b5b75e6b639b
cpvc <cpvc@openengine.dk>
2008-09-20 21:23:55 +0000

    added ogg to cmake file and updated to use new vorbis-win32.zip file

--- FindVorbis.cmake
+++ FindVorbis.cmake
@@ -20,27 +20,47 @@ FIND_PATH(VORBIS_INCLUDE_DIR vorbis/vorbisfile.h
   /opt/local/include
   /opt/csw/include
   /opt/include
-  ${OE_LIB_DIR}/soundlibs/include
+  ${OE_LIB_DIR}/vorbis/include
 )
 
 SET(VORBISLIB "vorbis")
 SET(VORBISFILELIB "vorbisfile")
+SET(OGGLIB "ogg")
 
 STRING(COMPARE EQUAL ${CMAKE_BUILD_TYPE} "debug" ISDEBUGENABLED)
 IF(CMAKE_BUILD_TOOL MATCHES "(msdev|devenv|nmake)")
   IF(ISDEBUGENABLED)
     SET(VORBISLIB "vorbis_static_d")
     SET(VORBISFILELIB "vorbisfile_static_d")
+    SET(OGGLIB "ogg_static_d")
   ELSE(ISDEBUGENABLED)
     SET(VORBISLIB "vorbis_static")
     SET(VORBISFILELIB "vorbisfile_static")
+    SET(OGGLIB "ogg_static")
   ENDIF(ISDEBUGENABLED)
 ENDIF(CMAKE_BUILD_TOOL MATCHES "(msdev|devenv|nmake)")
 
 FIND_LIBRARY(VORBISFILE_LIBRARY
   ${VORBISFILELIB}
   PATH
-  ${OE_LIB_DIR}/soundlibs/lib
+  ${OE_LIB_DIR}/vorbis/lib/static
+  $ENV{VORBIS_DIR}/lib
+  /usr/local/lib/vorbis
+  /usr/local/lib
+  /usr/lib/vorbis
+  /usr/lib
+  /sw/lib/vorbis
+  /sw/lib
+  /opt/local/lib/vorbis # DarwinPorts
+  /opt/local/lib
+  /opt/csw/lib
+  /opt/lib
+)
+
+FIND_LIBRARY(OGG_LIBRARY
+  ${OGGLIB}
+  PATH
+  ${OE_LIB_DIR}/vorbis/lib/static
   $ENV{VORBIS_DIR}/lib
   /usr/local/lib/vorbis
   /usr/local/lib
@@ -57,7 +77,7 @@ FIND_LIBRARY(VORBISFILE_LIBRARY
 FIND_LIBRARY(VORBIS_LIBRARY
   ${VORBISLIB}
   PATH
-  ${OE_LIB_DIR}/soundlibs/lib
+  ${OE_LIB_DIR}/vorbis/lib/static
   $ENV{VORBIS_DIR}/lib
   /usr/local/lib/vorbis
   /usr/local/lib
@@ -72,12 +92,12 @@ FIND_LIBRARY(VORBIS_LIBRARY
 )
 
 # Copy the results to the output variables.
-IF(VORBIS_INCLUDE_DIR AND VORBIS_LIBRARY AND VORBISFILE_LIBRARY)
+IF(VORBIS_INCLUDE_DIR AND VORBIS_LIBRARY AND VORBISFILE_LIBRARY AND OGG_LIBRARY)
   SET(VORBIS_FOUND 1)
-  SET(VORBIS_LIBRARIES ${VORBISFILE_LIBRARY} ${VORBIS_LIBRARY} )
+  SET(VORBIS_LIBRARIES ${VORBISFILE_LIBRARY} ${VORBIS_LIBRARY} ${OGG_LIBRARY})
   SET(VORBIS_INCLUDE_DIR ${VORBIS_INCLUDE_DIR})
-ELSE(VORBIS_INCLUDE_DIR AND VORBIS_LIBRARY AND VORBISFILE_LIBRARY)
+ELSE(VORBIS_INCLUDE_DIR AND VORBIS_LIBRARY AND VORBISFILE_LIBRARY AND OGG_LIBRARY)
   SET(VORBIS_FOUND 0)
   SET(VORBIS_LIBRARIES)
   SET(VORBIS_INCLUDE_DIR)
-ENDIF(VORBIS_INCLUDE_DIR AND VORBIS_LIBRARY AND VORBISFILE_LIBRARY)
+ENDIF(VORBIS_INCLUDE_DIR AND VORBIS_LIBRARY AND VORBISFILE_LIBRARY AND OGG_LIBRARY)
